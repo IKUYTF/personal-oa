eac57d7274aa5754ceb31635708bae7a9354ecad
IKUYTF <48087054+IKUYTF@users.noreply.github.com>
2020-01-17 13:57:08 +0900

    Add files via upload

---
+++ FrontEnd/personal-oa/README.md
@@ -0,0 +1,1 @@
+This is the first version of attendance management system, developed by vue.js/vuetify.
---
+++ FrontEnd/personal-oa/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+    host: 'localhost',
+    port: 8090
+  },
+    lintOnSave: false
+}
